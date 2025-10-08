319106bebbb2ff2b1c1ee13cea8ffd89b9b6f02e
SvetXi <svetamisaleva@mail.ru>
2025-10-08 20:16:22 +0500

    Update lesson05_task1.py

--- lesson05_task1.py
+++ lesson05_task1.py
@@ -2,7 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 
 driver = webdriver.Chrome()
-driver.get("http://uitestingplayground.com/dynamicid")
+driver.get("http://uitestingplayground.com/classattr")
 
 button = driver.find_element(By.CSS_SELECTOR, ".btn-primary")
 button.click()
