6546ca0368d8f4949b1ca1ccf181483a29b7c6f9
SvetXi <svetamisaleva@mail.ru>
2025-10-08 20:19:50 +0500

    Update lesson05_task4.py

--- lesson05_task4.py
+++ lesson05_task4.py
@@ -1,3 +1,7 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+driver = webdriver.Firefox()
 driver.get("http://the-internet.herokuapp.com/login")
 
 driver.find_element(By.ID, "username").send_keys("tomsmith")
