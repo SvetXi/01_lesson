99138b7bdc999889f1c77dc91ebf151c1ababe43
SvetXi <svetamisaleva@mail.ru>
2025-10-08 18:35:55 +0500

    Update lesson05_task3.py

--- lesson05_task3.py
+++ lesson05_task3.py
@@ -1,10 +1,12 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
-driver = webdriver.Chrome()
-driver.get("http://uitestingplayground.com/dynamicid")
+driver = webdriver.Firefox()
+driver.get("http://the-internet.herokuapp.com/inputs")
 
-button = driver.find_element(By.CSS_SELECTOR, ".btn-primary")
-button.click()
+input_field = driver.find_element(By.TAG_NAME, "input")
+input_field.send_keys("Sky")
+input_field.clear()
+input_field.send_keys("Pro")
 
 driver.quit()
