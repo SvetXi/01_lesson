c629c4d4afe98cc19e7dfd80f7c53d61d3daff58
SvetXi <svetamisaleva@mail.ru>
2025-10-15 10:29:20 +0500

    Update test_02_calc.py

--- test_02_calc.py
+++ test_02_calc.py
@@ -1,8 +1,8 @@
 import pytest
+import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
-from selenium.
-webdriver.chrome.options import Options as ChromeOptions
+from selenium.webdriver.chrome.options import Options as ChromeOptions
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
@@ -27,10 +27,10 @@ def test_slow_calc(driver):
     delay_input.send_keys("45")
 
     # Нажимаем кнопки: 7 + 8 =
-    driver.find_element(By.CSS_SELECTOR, "button[value='7']").click()
-    driver.find_element(By.CSS_SELECTOR, "button[value='+']").click()
-    driver.find_element(By.CSS_SELECTOR, "button[value='8']").click()
-    driver.find_element(By.CSS_SELECTOR, "button[value='=']").click()
+    driver.find_element(By.CSS_SELECTOR, "button.btn[data-value='7']").click()
+    driver.find_element(By.CSS_SELECTOR, "button.btn[data-value='+']").click()
+    driver.find_element(By.CSS_SELECTOR, "button.btn[data-value='8']").click()
+    driver.find_element(By.CSS_SELECTOR, "button.btn[data-value='=']").click()
 
     # Ожидаем текст "15" в поле с результатом (input#result)
     result = wait.until(EC.text_to_be_present_in_element_value((By.CSS_SELECTOR, "input#result"), "15"))
