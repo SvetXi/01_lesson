25e047a8d972584081fc4201f31e442f222243fe
SvetXi <svetamisaleva@mail.ru>
2025-10-28 09:02:51 +0500

    Update test_calc.py

--- test_calc.py
+++ test_calc.py
@@ -2,7 +2,7 @@ from time import sleep
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
-from Lesson_07.calc_02.pages.CalcPage import CalcPage
+from Lesson_07.CalcPage import CalcPage
 
 
 def test_calculator():
